ff65feb3847b4b0db01d8aa1e7d39f00b49ed909
farzeen-ali <farzeenaliarif786@gmail.com>
2025-07-16 10:05:15 -0700

    Array Basics

---
+++ ArrayBasics.java
@@ -0,0 +1,19 @@
+public class ArrayBasics {
+    public static void main(String[] args) {
+//        int[] numbers; //Declare
+//        numbers = new int[5]; //Initialize
+
+//        int[] numbers = new int[5]; //Declaration + Size Initialization
+
+        int[] numbers = {10,20,30,40,50};
+
+        // Insert
+//        numbers[0] = 10;
+//        numbers[1] = 20;
+//        numbers[2] = 30;
+//        numbers[3] = 40;
+//        numbers[4] = 50;
+
+        System.out.println("Element at index 4: " + numbers[4]);
+    }
+}
