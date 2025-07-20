a04387e41097d9170cb45063b1167f3429f280f7
farzeen-ali <farzeenaliarif786@gmail.com>
2025-07-20 11:25:01 -0700

    Array Traversing

---
+++ ArrayTraversing.java
@@ -0,0 +1,26 @@
+public class ArrayTraversing {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+//        Access an element using index
+//        System.out.println("Element at index 3: " + numbers[3]);
+
+//        Traversing an Array
+//        System.out.println("Traversing an Array: ");
+//        for (int i = 0; i < numbers.length; i++){
+//            System.out.println("Element at index " + i + ": " + numbers[i]);
+//        }
+
+//        Traversing an array in reverse order
+//        System.out.println("Traversing an Array in reverse order: ");
+//        for (int i = numbers.length - 1; i >= 0 ; i--){
+//            System.out.println("Element at index " + i + ": " + numbers[i]);
+//        }
+
+//        For Each
+//        for(int num : numbers){
+//            System.out.println("Element: " + num);
+//        }
+
+    }
+}
